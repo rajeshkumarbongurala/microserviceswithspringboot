1e101568c8c5ae9afaeb02aed3023749d3eac05c
RajeshKumar B <rbongurala@innominds.com>
2019-02-02 15:56:45 +0530

    Spring boot micro services Initial Commit

---
+++ MicroService2/src/main/java/com/example/MicroService2/MicroService2Application.java
@@ -0,0 +1,16 @@
+package com.example.MicroService2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class MicroService2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroService2Application.class, args);
+	}
+
+}
+
---
+++ MicroService3/src/main/java/com/example/MicroService3/MicroService3Application.java
@@ -0,0 +1,14 @@
+package com.example.MicroService3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroService3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroService3Application.class, args);
+	}
+
+}
+
